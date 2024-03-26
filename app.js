bfca2392859d6380e43e281015314456188ae5a4
Martín Sarasola <martin.sarasola01@hotmail.com>
2024-03-25 22:19:31 -0300

    added icon change

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ async function checkWeather(city) {
     console.log(data2);
 
     const name = document.querySelector(".name");
+    const icon = document.querySelector(".wicon");
     const degree = document.querySelector(".degree");
     const humidity = document.querySelector(".precipitation");
     const wind = document.querySelector(".wind"); 
     const lat = data2[0].lat;
     const lon = data2[0].lon;
+
     console.log(lat);
     console.log(lon);
 
@@ -28,22 +30,69 @@ async function checkWeather(city) {
     var data3 = await response3.json();
     console.log(data3);
 
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m&timezone=auto`);
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m&timezone=auto`);
 
     var data = await response.json();
 
     console.log(data);
 
+    const weatherCode = data.current.weather_code;
+
+    console.log(weatherCode);
+
     name.innerHTML = data3.features[0].properties.state + ", " + data3.features[0].properties.country;
     degree.innerHTML = Math.round(data.current.temperature_2m) + "°C";
     humidity.innerHTML = "Precipitación" + "<br>" + Math.floor(data.current.precipitation) + "%";
     wind.innerHTML = "Viento" + "<br>" + Math.floor(data.current.wind_speed_10m) + " Km/h";
+    
+    switch (weatherCode) {
+        case 0:
+            icon.src = "./images/sun.png";
+            break;
+        case 1:
+        case 2:
+        case 3:
+            icon.src = "./images/suncloud.png";
+            break;
+        case 45:
+        case 48:
+        case 51:
+            icon.src = "./images/cloud.png"
+            break;
+        case 53:
+        case 55:
+        case 56:
+        case 57:
+        case 61:
+        case 63:
+        case 65:
+        case 66:
+        case 67:
+        case 80:
+        case 81:
+        case 82:
+            icon.src = "./images/rain.png";
+            break;
+        case 71:
+        case 73:
+        case 75:
+        case 77:
+        case 85:
+        case 86:
+            icon.src ="./images/snowcloud.png";
+            break;
+        case 95:
+        case 96:
+        case 99:
+            icon.src ="./images/lightrain.png"
+            break;
+    };
 
-    card.style.height = '70vh';
+    card.style.height = null;
+    card.style.maxHeight = '90vh';
 }
 
-
-card.style.height = '5vh';
+card.style.maxHeight = '5vh';
 
 inputButton.addEventListener('click', () => {
     checkWeather(inputBox.value);
